07072431ae1951a60c2e630b1a0ba673c9e9ef7d
이석호 <mercen@mercen.net>
2022-05-12 10:47:00 +0900

    v0.3

--- asserDB.py
+++ asserDB.py
@@ -1,5 +1,5 @@
 '''
-asserDB v0.2 developed by Seok Ho Lee
+asserDB v0.3 developed by Seok Ho Lee
 (A.K.A. Mercen Lee)
 Github : https://github.com/Mercen-Lee
 Tistory : https://mercen.net/
@@ -13,7 +13,7 @@ def typer(variable):
         except: float(variable); return 'float'
     except: return 'str'
 
-def read(filename):
+def reader(filename):
     result={};temp=[];file=open(filename,'r');inside=file.read()
     if inside.startswith('asserDB;'):inside=inside[8:]
     else: Exception('Database is Damaged.')
@@ -21,31 +21,37 @@ def read(filename):
     for line in dc.splitlines():
         for crop in line.split(':'):temp.append(crop[1:-1])
         for i in range(0,2):
-            if typer(temp[i])=='int':temp[i]=int(temp[i])
-            elif typer(temp[i])=='float':temp[i]=float(temp[i])
-            else:temp[i]=temp[i][1:-1]
+            try:
+                try:temp[i]=int(temp[i])
+                except:temp[i]=float(temp[i])
+            except:temp[i]=temp[i][1:-1]
         result[temp[0]]=temp[1];temp=[]
     file.close();return result
 
-def write(dictionary, filename):
+def writer(dictionary,filename):
     dckeys=list(dictionary.keys());dcvals=list(dictionary.values());dc=[]
     for i in range(0,len(dckeys)):
-        if typer(dckeys[i])!='str':dckeys[i]='['+str(dckeys[i])+']:'
+        if type(dckeys[i])!=str:dckeys[i]='['+str(dckeys[i])+']:'
         else:dckeys[i]='[\''+dckeys[i]+'\']:'
-        if typer(dcvals[i])!='str':dcvals[i]='['+str(dcvals[i])+']'
+        if type(dcvals[i])!=str:dcvals[i]='['+str(dcvals[i])+']'
         else:dcvals[i]='[\''+dcvals[i]+'\']'
         dc.append(dckeys[i]+dcvals[i])
     ec=base64.b64encode('\n'.join(dc).encode('utf-8')).decode('utf-8')
     file=open(filename,'w');file.write('asserDB;'+ec);file.close() 
 
-def check():
-    asLink=path.basename(stack()[2].filename.replace('.py',''))
-    asName='.asserDB$'+path.splitext(asLink)[0]
-    if not path.isfile(asName):open(asName,'w+').close()
-    try:run(['attrib','+h',asName],check=True)
-    except: pass
-    return asName
+def check(name,stk):
+    if not name:name=path.basename(stack()[stk].filename.replace('.py',''))
+    link='.asserDB$'+path.splitext(name)[0]
+    if not path.isfile(link):open(link,'w+').close()
+    try:run(['attrib','+h',link],check=True)
+    except:pass
+    return link
 
-def readDict():return read(check())
+def readDict(name=None,stk=2):return reader(check(name,stk))
 
-def writeDict(dictionary):write(dictionary,check())
+def writeDict(dictionary,name=None,stk=2):writer(dictionary,check(name,stk))
+
+def read(key,name=None):return readDict(name,3)[key]
+
+def write(key,value,name=None):
+    dc=readDict(name,3);dc[key]=value;writeDict(dc,name,3)
